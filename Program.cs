82e02484824526189c108fa30ebdfd19242a5996
Robert O'Brien <myusrn@outlook.com>
2017-08-11 11:10:52 -0700

    initial checkin

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapMouseButton
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SwapMouseButton([param: MarshalAs(UnmanagedType.Bool)] bool fSwap);
+
+        enum MouseButtonsSetting
+        {
+            RightHanded = 0,
+            LeftHanded = 1
+        }        
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // implementation if you just want to set mouse button settings to left handed if 
+            // no arguments are found and set to right handed if /u argument included
+            //if (args.Length > 0 && String.Compare(args[0], "/u", true) == 0) SwapMouseButton(0);
+            //else SwapMouseButton(1);
+
+            // implementation if you want to always swap mouse button settings from current state
+            var currentSetting = GetMouseButtonsSetting();
+
+            if (currentSetting == MouseButtonsSetting.RightHanded) SetMouseButtonsSetting(MouseButtonsSetting.LeftHanded);
+            else /* (currentSetting == MouseButtonSettings.LeftHanded) */ SetMouseButtonsSetting(MouseButtonsSetting.RightHanded);
+        }
+
+        /// <summary>
+        /// gets the mouse buttons setting currently in effect
+        /// </summary>
+        /// <returns>value indicating whether right or left handed setting is in place</returns>
+        static MouseButtonsSetting GetMouseButtonsSetting()
+        {
+            var mouseButtonsSetting = MouseButtonsSetting.RightHanded;
+
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Mouse"))
+            {
+                if (key is null) throw new ApplicationException("unable to open mouse settings registry key");
+                var kv = key.GetValue("SwapMouseButtons");
+                if (kv is null) throw new ApplicationException("unable to open mouse settings registry key value");
+                if (Convert.ToInt16(kv) == 1) mouseButtonsSetting = MouseButtonsSetting.LeftHanded;
+            }
+
+            return mouseButtonsSetting;
+        }
+
+        /// <summary>
+        /// sets the mouse buttons setting currently in effect
+        /// </summary>
+        /// <returns>value indicating whether right or left handed setting is in place</returns>
+        static void SetMouseButtonsSetting(MouseButtonsSetting mouseButtonsSetting)
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Mouse"))
+            {
+                if (key is null) throw new ApplicationException("unable to open mouse settings registry key");
+                if (mouseButtonsSetting == MouseButtonsSetting.RightHanded)
+                {
+                    SwapMouseButton(false); // change runtime setting
+                    try { key.SetValue("SwapMouseButtons", "0", RegistryValueKind.String); } // change persisted setting
+                    catch (UnauthorizedAccessException) { Console.WriteLine(
+                        "unable to persist change execute from \"run as administrator\" environment"); }
+                }
+                else /* (mouseButtonsSetting == MouseButtonSettings.LeftHanded) */
+                {
+                    SwapMouseButton(true); // change runtime setting
+                    try { key.SetValue("SwapMouseButtons", "1", RegistryValueKind.String); } // change persisted setting
+                    catch (UnauthorizedAccessException) { Console.WriteLine(
+                        "unable to persist change execute from \"run as administrator\" environment"); }
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# swapMouseButton
-command line utility to switch primary and secondary mouse buttons
+# SwapMouseButtons
+This is a command line utility to switch primary and secondary mouse buttons. I regularly
+switch sides with my mouse to try and avoid repetitive posture related issues while working
+in front of the box. This gave me a build window command line story for switching mouse 
+buttons without having to navigate to the mouse properties gui.   
+
+It uses a registry lookup to determine what mouse buttons setting is currently in place then
+a managed code api wrapper call to the win32 SwapMouseButton api to switch the setting to the
+opposite of whatever it currently is.
+
+reg query "hkcu\Control Panel\Mouse" /v SwapMouseButtons 
+- reg_sz = 0 = right handed mouse buttons setting is enabled
+- reg_sz = 1 = left handed mouse buttons setting is enabled
+
+reg add "hkcu\Control Panel\Mouse" /v SwapMouseButtons /t reg_sz /d 0|1
+- this doesn't apply setting until you signout and back in again presumably 
+because that triggers mouse button settings lookup and initialization
+
+Calling SwapMouseButton win32 api changes what is currently in effect but does
+not change the registry setting. So that needs to be modified as well otherwise
+the next time you open and close mouse settings control panel, main.cpl, the
+setting will revert to what the registry setting is configured to be.
+&nbsp;  
+
+---
+# References
+1. "switch mouse buttons command line utility" ->
+- https://stackoverflow.com/questions/4806575/how-do-i-use-rundll32-to-swapmousebutton
+- https://superuser.com/questions/857259/how-do-you-swap-the-primary-mouse-button-via-commandline-in-windows-8-without-a
+- https://www.codeproject.com/Articles/66272/Programmatically-Swapping-Mouse-Buttons
+  
+2. "c# read registry key value" -> 
+- https://stackoverflow.com/questions/18232972/how-to-read-value-of-a-registry-key-c-sharp  
+
+3. "SwapMouseButton win32 api" -> 
+- https://msdn.microsoft.com/en-us/library/ms646264(VS.85).aspx  
+ 
+4. "which open source license should i use" -> 
+- https://whichopensourcelicense.com/  
